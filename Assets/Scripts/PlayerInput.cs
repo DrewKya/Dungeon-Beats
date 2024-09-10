9d380766852543150691c64f3e340a02aadc8985
DrewKya <145177408+DrewKya@users.noreply.github.com>
2024-09-10 16:04:14 +0700

    a

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    [SerializeField] Transform groundCheck;
 
+    GameObject currentPositionTile;
+
+    private void Start()
+    {
+        CheckGround(groundCheck.position);
+    }
 
     private void Update()
     {
         CheckInput();
+        Debug.Log(currentPositionTile.transform.position);
     }
 
     private void CheckInput()
@@ -17,31 +25,50 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W)) //move up
         {
             transform.position += new Vector3(0, 0, 1);
-            
+            CheckGround(groundCheck.position);
         }
         else if (Input.GetKeyDown(KeyCode.S)) //move down
         {
             transform.position += new Vector3(0, 0, -1);
+            CheckGround(groundCheck.position);
         }
         else if (Input.GetKeyDown(KeyCode.D)) //move right
         {
             transform.position += new Vector3(1, 0, 0);
+            CheckGround(groundCheck.position);
         }
         else if (Input.GetKeyDown(KeyCode.A)) //move left
         {
             transform.position += new Vector3(-1, 0, 0);
+            CheckGround(groundCheck.position);
         }
     }
 
     private void CheckGround(Vector3 origin)
     {
         RaycastHit hit;
-        if(Physics.Raycast(origin, Vector3.down, out hit))
-        {
-            if (hit.collider == null) //note : bikin buat check ground, bikin merah
-            {
 
+        if (Physics.Raycast(origin, Vector3.down, out hit, 1f, LayerMask.GetMask("Ground")))
+        {
+            //Debug.DrawRay(origin, Vector3.down * 1f, Color.yellow);
+
+            GameObject obj = hit.collider.gameObject;
+
+            currentPositionTile.GetComponentInChildren<Renderer>().material.color = Color.green;
+
+            currentPositionTile = obj;
+
+            if (obj.CompareTag("Ground"))
+            {
+                Renderer renderer = obj.GetComponentInChildren<Renderer>();
+                if (renderer != null)
+                {
+                    renderer.material.color = Color.red;
+                }
             }
+
+            
         }
+
     }
 }

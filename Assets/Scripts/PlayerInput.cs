3e698c6baad59ec1985458ec2adf61288f378cb9
Kevinjomin <122532032+Kevinjomin@users.noreply.github.com>
2024-09-10 19:08:38 +0700

    updated movement checks

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class PlayerInput : MonoBehaviour
 {
@@ -16,32 +18,45 @@ public class PlayerInput : MonoBehaviour
 
     private void Update()
     {
-        CheckInput();
+        CheckMovementInput();
         Debug.Log(currentPositionTile.transform.position);
     }
 
-    private void CheckInput()
+    private void CheckMovementInput()
     {
+        Vector3 positionIncrement = new Vector3(0, 0, 0);
         if (Input.GetKeyDown(KeyCode.W)) //move up
         {
-            transform.position += new Vector3(0, 0, 1);
-            CheckGround(groundCheck.position);
+            positionIncrement = new Vector3(0, 0, 1);
         }
         else if (Input.GetKeyDown(KeyCode.S)) //move down
         {
-            transform.position += new Vector3(0, 0, -1);
-            CheckGround(groundCheck.position);
+            positionIncrement = new Vector3(0, 0, -1);
         }
         else if (Input.GetKeyDown(KeyCode.D)) //move right
         {
-            transform.position += new Vector3(1, 0, 0);
-            CheckGround(groundCheck.position);
+            positionIncrement = new Vector3(1, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.A)) //move left
         {
-            transform.position += new Vector3(-1, 0, 0);
-            CheckGround(groundCheck.position);
+            positionIncrement = new Vector3(-1, 0, 0);        
         }
+
+        if(positionIncrement.magnitude > 0) //if a movement input is detected
+        {
+            Vector3 targetTilePosition = groundCheck.position + positionIncrement;
+            if (CheckIfWalkable(targetTilePosition))
+            {
+                transform.position += positionIncrement;
+                CheckGround(groundCheck.position);
+            }
+        }   
+    }
+
+    private bool CheckIfWalkable(Vector3 target)
+    {
+        RaycastHit hit;
+        return (Physics.Raycast(target, Vector3.down, out hit, 1f, LayerMask.GetMask("Ground"))) ? true : false;
     }
 
     private void CheckGround(Vector3 origin)
@@ -53,11 +68,14 @@ public class PlayerInput : MonoBehaviour
             //Debug.DrawRay(origin, Vector3.down * 1f, Color.yellow);
 
             GameObject obj = hit.collider.gameObject;
-
-            currentPositionTile.GetComponentInChildren<Renderer>().material.color = Color.green;
-
+            /*
+            if (currentPositionTile != null)
+            {
+                currentPositionTile.GetComponentInChildren<Renderer>().material.color = Color.white;
+            }
+            */
             currentPositionTile = obj;
-
+            /*
             if (obj.CompareTag("Ground"))
             {
                 Renderer renderer = obj.GetComponentInChildren<Renderer>();
@@ -66,7 +84,7 @@ public class PlayerInput : MonoBehaviour
                     renderer.material.color = Color.red;
                 }
             }
-
+            */
             
         }
 

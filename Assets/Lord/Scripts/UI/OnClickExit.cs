c40665f795749ba59a031a1ec46e444565a736e4
Kevinjomin <122532032+Kevinjomin@users.noreply.github.com>
2024-12-05 11:56:32 +0700

    Updated hub layout

--- Assets/Lord/Scripts/UI/OnClickExit.cs
+++ Assets/Lord/Scripts/UI/OnClickExit.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 //exits the game when this object is clicked
 public class OnClickExit : MonoBehaviour
 {
+    private void Start()
+    {
+        if(GetComponent<Button>() != null)
+        {
+            GetComponent<Button>().onClick.AddListener(() => QuitGame());
+        }
+    }
+
     private void OnMouseDown()
+    {
+        QuitGame();
+    }
+
+    private void QuitGame()
     {
         GameManager.instance.QuitApplication();
     }
